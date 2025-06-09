afba9e5c06cee64e8e00f359f3ce8d54c52f729a
mkashifshah <kashiflss99@gmail.com>
2025-06-09 13:11:06 +0500

    Create FriendRequest.kt

---
+++ app/src/main/java/com/example/boomerangapp/model/FriendRequest.kt
@@ -0,0 +1,8 @@
+package com.example.boomerangapp.model
+
+data class User(
+    val id: String = "",
+    val username: String = "",
+    val email: String = "",
+    val profileImageUrl: String = ""
+)
